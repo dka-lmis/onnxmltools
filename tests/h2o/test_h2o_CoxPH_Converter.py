2138827a92219e8978575223d9e1b9b756b04dc6
Daniel Kamphake <Daniel.Kamphake@lmis.de>
2022-04-20 16:46:21 +0200

    Update test_h2o_CoxPH_Converter.py

--- tests/h2o/test_h2o_CoxPH_Converter.py
+++ tests/h2o/test_h2o_CoxPH_Converter.py
@@ -3,34 +3,33 @@
 """
 Tests H2O Distributed Random Forest (DRF) converter
 """
-import os
 import unittest
 
-from h2o import h2o
+from h2o.exceptions import H2OError
 from onnx.defs import onnx_opset_version
 from onnxconverter_common import DEFAULT_OPSET_NUMBER
-
-from onnxmltools.convert.h2o import convert
 from onnxmltools.utils import dump_data_and_model
+
+from h2o import h2o
 from h2o.estimators.coxph import H2OCoxProportionalHazardsEstimator
 
-'''TARGET_OPSET = min(DEFAULT_OPSET_NUMBER, onnx_opset_version())
+from tests.h2o.h2o_train_util import _convert_mojo, _train_and_get_model_path, H2OMojoWrapper
+TARGET_OPSET = min(DEFAULT_OPSET_NUMBER, onnx_opset_version())
+
+
+def _get_CoxPH_dataset():
+    # Import the heart dataset into H2O:
+    heart = h2o.import_file("http://s3.amazonaws.com/h2o-public-test-data/smalldata/coxph_test/heart.csv")
+
+    # Split the dataset into a train and test set:
+    train, valid = heart.split_frame(ratios=[.8], seed=1234)
+    x = 'age'
+    y = 'event'
+
+    return x, y, train, valid
 
 
 class H2OTestConverterCoxPH(unittest.TestCase):
-
-    @staticmethod
-    def setup():
-        # Import the heart dataset into H2O:
-        heart = h2o.import_file("http://s3.amazonaws.com/h2o-public-test-data/smalldata/coxph_test/heart.csv")
-
-        # Split the dataset into a train and test set:
-        train, _ = heart.split_frame(ratios=[.8], seed=1234)
-
-        folder = os.environ.get('ONNXTESTDUMP', 'tests/temp')
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-        return train, folder
 
     @classmethod
     def setUpClass(cls):
@@ -40,22 +39,31 @@ class H2OTestConverterCoxPH(unittest.TestCase):
     def tearDownClass(cls):
         h2o.cluster().shutdown()
 
-    def test_h2o_algo_support(self):
-        train, folder = self.setup()
+    @unittest.skip(reason='''H2O does not generates an empty file when downloading a CoxPH Model. The model generates 
+                          a correct zip-file with the data of the model though.''')
+    def test_h2o_CoxPH_algo_support(self):
+        x, y, train, test = _get_CoxPH_dataset()
         model = H2OCoxProportionalHazardsEstimator(start_column="start",
                                                    stop_column="stop",
                                                    ties="breslow")
-        model.train(x="age",
-                    y="event",
-                    training_frame=train)
-
-        mojo_path = model.download_mojo(path=folder)
-        print(mojo_path)
-        with self.assertRaises(ValueError) as err:
+        mojo_path = _train_and_get_model_path(model, x, y, train, test)
+        with self.assertRaises(H2OError) as err:
             _convert_mojo(mojo_path)
-        self.assertRegex(err.exception.args[0], "not supported")
+        self.assertRegex(err.exception.args[0], "Unable to print")
+
+    @unittest.skip(reason='not yet implemented')
+    def test_h2o_CoxPH_conversion(self):
+        x, y, train, test = _get_CoxPH_dataset()
+        model = H2OCoxProportionalHazardsEstimator(start_column="start",
+                                                   stop_column="stop",
+                                                   ties="breslow")
+        mojo_path = _train_and_get_model_path(model, x, y, train, test)
+        onnx_model = _convert_mojo(mojo_path)
+        self.assertIsNot(onnx_model, None)
+        dump_data_and_model(
+            test, H2OMojoWrapper(mojo_path),
+            onnx_model, basename="H2OReg-Dec4")
 
 
 if __name__ == "__main__":
     unittest.main()
-'''
